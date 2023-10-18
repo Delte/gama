01f3d084a18bfa87a9265b743b3077be217b76aa
N. Emre Özbakır <nurettinemreozbakir@gmail.com>
2023-10-18 16:42:21 +0300

    fixes for new DocShare

--- gama/gama_settings/doctype/gama_drive_settings/gama_drive_settings.js
+++ gama/gama_settings/doctype/gama_drive_settings/gama_drive_settings.js
@@ -2,7 +2,25 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Gama Drive Settings', {
-	// refresh: function(frm) {
-
-	// }
+	refresh(frm) {
+	},
+	create(frm){
+		console.log(frm.doc.project_folder_create);
+		frappe.call({
+			method: "gama.api.drive.utils.manage_project_folders",
+			args: {
+				project: frm.doc.project_folder_create
+			}
+		});
+	},
+	remove(frm){
+		console.log(frm.doc.project_folder_remove);
+		frappe.call({
+			method: "gama.api.drive.utils.manage_project_folders",
+			args: {
+				project: frm.doc.project_folder_remove,
+				operation: 'remove'
+			}
+		});
+	}
 });
